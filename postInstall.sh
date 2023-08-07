d3d88eea32127420e32528fc013bf53655b93565
zyzyx159 <40427026+zyzyx159@users.noreply.github.com>
2023-08-06 22:34:45 -0500

    Update postInstall.sh
    nixos needs to be in lower case

--- postInstall.sh
+++ postInstall.sh
@@ -4,4 +4,4 @@ cd ~/git
 nix-shell -p git --run "git clone https://github.com/zyzyx159/nixos.git"
 sudo rm /etc/nixos/configuration.nix
 sudo ln -s ~/git/nixos/configuration.nix /etc/nixos/configuration.nix
-sudo NixOS-rebuild switch
+sudo nixos-rebuild switch

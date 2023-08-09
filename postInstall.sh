33a30e84ade47fb8a1c0057959d9802ed76d1207
Daniel Stellmon <dstellm@gmail.com>
2023-08-09 05:32:43 -0500

    added reboot to postinstall.sh

--- postInstall.sh
+++ postInstall.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-# install home manager channel
+#Install home manager channel
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
 nix-channel --update
 
-# add home manager to the path
+#Add home manager to the path
 export NIX_PATH="$HOME/.nix-defexpr/channels${NIX_PATH:+:$NIX_PATH}"
 
-# run the nixos home_manger install
+#Run the nixos home_manger install
 nix-shell '<home-manager>' -A install
 
-# make a git folder
+#Make a git folder
 mkdir ~/git
 cd ~/git
 
-# pull down my nixos configs
+#Pull down my nixos configs
 nix-shell -p git --run "git clone https://github.com/zyzyx159/nixos.git"
 
-#remove old nixos config and replace it with git
+#Remove old nixos config and replace it with git
 sudo rm /etc/nixos/configuration.nix
 sudo ln -s ~/git/nixos/configuration.nix /etc/nixos/configuration.nix
 
-# remove old home manager config and replace it
-# *** this script is designed for single user systems. Changes will need to be made if you want more than one home-manager config.***
+#Remove old home manager config and replace it
+#*** this script is designed for single user systems. Changes will need to be made if you want more than one home-manager config.***
 rm ~/.config/home-manager/home.nix
 ln -s ~/git/nixos/home.nix ~/.config/home-manager/home.nix
 
+#Run the NixOS Installer
 sudo nixos-rebuild switch
+
+#Reboot the PC
+sudo reboot -h now

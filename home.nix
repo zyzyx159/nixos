10fdc289c2d6e4c34b140788f173f2056f44037d
Daniel Stellmon <DStellm@gmail.com>
2023-08-23 04:05:55 -0500

    editing starship

--- home.nix
+++ home.nix
@@ -1,40 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "zyzyx";
   home.homeDirectory = "/home/zyzyx";
   home.sessionPath = [ "~/.local/bin" ];
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "23.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
     cargo
     git
     gnumake
@@ -64,7 +37,7 @@
     settings = {
       add_newline = true;
       command_timeout = 1000;
-      format = "[╭╴](white)$env_var $all[╰─](white)$character";
+      format = "$username";
       character = {
         success_symbol = "[󰜃 ](bright-cyan)";
         error_symbol = "[](red)";
